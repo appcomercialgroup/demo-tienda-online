83c304cf0b18f4a8455650de2e6d9b4b38e4f496
Carlos Aleman <caleman9791@gmail.com>
2024-02-07 02:27:48 -0400

    Confirmación de cambios iniciales.

---
+++ js/js/js.js
@@ -0,0 +1,9 @@
+var buttonHolder = document.getElementById("buttonHolder");
+
+// buttonHolder.addEventListener("click", function (argument) {
+// 	argument.preventDefault();
+// });
+buttonHolder.addEventListener("click", function (argument) {
+	// body...
+	argument.preventDefault();
+});
