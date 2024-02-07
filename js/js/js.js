bb59a74ac81bb43b168344a4ae1949ff714f7f0e
Carlos Aleman <caleman9791@gmail.com>
2024-02-07 07:48:21 -0400

    Confirmación modal de ejemplo

--- js/js/js.js
+++ js/js/js.js
@@ -1,4 +1,4 @@
-var buttonHolder = document.getElementById("buttonHolder");
+let buttonHolder = document.getElementById("buttonHolder");
 
 // buttonHolder.addEventListener("click", function (argument) {
 // 	argument.preventDefault();
@@ -7,3 +7,22 @@ buttonHolder.addEventListener("click", function (argument) {
 	// body...
 	argument.preventDefault();
 });
+
+let codemodal = document.getElementById("codemodal");
+
+let codemodalccion = document.querySelectorAll(".codemodalccion");
+
+for (var i = 0; i < codemodalccion.length; i++) {
+	codemodalccion[i].addEventListener("click", function (argument) {
+		// body...
+		argument.preventDefault();
+		let testodelboton = document.querySelector(".testodelboton");
+		let textoBoton = argument.target.textContent.trim();
+		testodelboton.textContent = textoBoton;
+		codemodal.style.display = "block";
+		// block
+		// console.log(argument.target.textContent.trim());
+		// argument.target
+	});
+
+}
